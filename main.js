cc72d5c9b1d8e12c606433da0f08b2f9e132f27e
Rajdeep Das <r4das@ucsd.edu>
2022-07-07 19:13:49 -0700

    added minimal web ui.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { Client } = require('tplink-smarthome-api');
+const { Server } = require('socket.io');
 const fs = require('fs');
 const http = require('http');
+const static = require('node-static');
+const Router = require('node-router');
 const noble = require('noble-winrt');
 
 const config = JSON.parse(fs.readFileSync("config.json"));
@@ -18,8 +21,12 @@ const HR_MAX = 220;
 const DEVMASK_PLUG = 1;
 const DEVMASK_HRM = 2;
 
+const LISTEN_PORT = 3000;
+
 var hrm = null;
 var plug = null;
+
+var sock = null;
 
 var initDev = 0;
 
@@ -30,11 +37,17 @@ var isOn = false;
 const client = new Client();
 
 function switchOnFan() {
-    if(plug != null && !isOn) plug.setPowerState(true);
+    if(plug != null && !isOn) {
+        plug.setPowerState(true);
+        if(sock != null) sock.emit('fan', { 'err' : null, 'data' : 'on' });
+    }
 }
 
 function switchOffFan() {
-    if(plug != null && isOn) plug.setPowerState(false);
+    if(plug != null && isOn) {
+        plug.setPowerState(false);
+        if(sock != null) sock.emit('fan', { 'err' : null, 'data' : 'off' });
+    }
 }
 
 function checkInit(devMask) {
@@ -84,6 +97,10 @@ noble.on('discover', function(peripheral) {
                             }
                             var buflen = data.length;
                             var hrate = (buflen == 2) ? data.readInt16BE() : (data.readInt32BE() & 0x00FF0000) >> 16;
+                            if(sock != null) {
+                                sock.emit('hr', { 'err' : null, 'data' : hrate });
+                                sock.emit('fan', { 'err' : null, 'data' : (isOn ? 'on' : 'off') });
+                            }
                             if(hrate != currHR && hrate < HR_MAX && hrate > 0) {
                                 currHR = hrate;
                                 if(hrate > HR_THRESH_HIGH) {
@@ -107,25 +124,57 @@ noble.on('discover', function(peripheral) {
 
 noble.startScanning();
 
-/*const requestListener = function (req, res) {
-    var cmd = req.url.split('/')[1];
-    res.writeHead(200);
-    if(cmd === 'on') {
-        if(plug._sysInfo.relay_state == 0) {
-            console.log('Powered off.');
-            plug.setPowerState(true);
-        }
-        res.end('OK.');
-    } else if(cmd === 'off') {
-        if(plug._sysInfo.relay_state == 1) {
-            console.log('Powered on.');
-            plug.setPowerState(false);
-        }
-        res.end('OK.');
-    } else {
-        res.end('Unknown command.');
-    }
-}
+const localfs = new(static.Server)('static');
+const router = Router();
+const route = router.push;
 
-const server = http.createServer(requestListener);
-server.listen(3000);*/
+route('/on', function(req, res, next) {
+    if(plug._sysInfo.relay_state == 0) {
+        switchOnFan();
+        res.send(JSON.stringify({
+            'success'   : true,
+            'err'       : null
+        }));
+    } else {
+        res.send(JSON.stringify({
+            'success'   : false,
+            'err'       : 'already running'
+        }));
+    }
+});
+
+route('/off', function(req, res, next) {
+    if(plug._sysInfo.relay_state == 1) {
+        switchOffFan();
+        res.send(JSON.stringify({
+            'success'   : true,
+            'err'       : null
+        }));
+    } else {
+        res.send(JSON.stringify({
+            'success'   : false,
+            'err'       : 'already stopped'
+        }));
+    }
+});
+
+route(function(req, res, next) {
+    localfs.serve(req, res);
+});
+
+route(function(err, req, res, next) {
+    res.send(JSON.stringify(err));
+});
+
+const server = http.createServer(router);
+const io = new Server(server);
+
+io.on('connection', function(socket) {
+    sock = socket;
+    sock.emit('fan', { 'err' : null, 'data' : (isOn ? 'on' : 'off') });
+    console.log('Connected to web interface.');
+});
+
+server.listen(LISTEN_PORT, function() {
+    console.log('Listening on *:', LISTEN_PORT);
+});
