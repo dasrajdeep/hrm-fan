bb764ee4ad14cf6152d868515291cc007cd932ef
Rajdeep Das <das.rajdeep97@gmail.com>
2022-07-25 23:22:45 -0700

    ui for ble sensors/actuators.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const DEBUG = true;
+
 const { Client } = require('tplink-smarthome-api');
 const { Server } = require('socket.io');
 const fs = require('fs');
@@ -8,22 +10,32 @@ const noble = require('@abandonware/noble');
 
 const config = JSON.parse(fs.readFileSync("config.json"));
 
-const FAN_PLUG_DEVID = config['plug-devid'];
+const DEFAULT_THRESHOLDS = {
+    'HR_TRIGGER'    : 170,
+    'HR_MAX'        : 220
+};
 
-const HRM_DUAL_UUID = config['hrm-uuid'];
-const HRM_DUAL_MAC = config['hrm-mac'];
-const HR_THRESH_LOW = config['hr-thres-low'];
-const HR_THRESH_HIGH = config['hr-thres-high'];
+const SERVICE_UUIDS = {
+    '180d'  : 'HEART_RATE',
+    '1816'  : 'SPEED_CADENCE',
+    '1818'  : 'POWER'
+};
 
-const SERVICE_UUID_HRM = "180d";
-const CHARACTERISTIC_UUID_HRM = "2a37";
-const HR_MAX = 220;
-const DEVMASK_PLUG = 1;
-const DEVMASK_HRM = 2;
+const CHARACTERISTIC_UUIDS = {
+    '2a37'  : 'HEART_RATE_MEASUREMENT',
+    '2a19'  : 'BATTERY_LEVEL',
+    '2a38'  : 'BODY_SENSOR_LOCATION',
+    '2a63'  : 'CYCLING_POWER_MEASUREMENT'
+};
+
+const SERVICE_DESCRIPTIONS = {
+    'HEART_RATE'    : 'Heart Rate Monitor',
+    'SPEED_CADENCE' : 'Speed/Cadence Sensor',
+    'POWER'         : 'Power Meter'
+};
 
 const LISTEN_PORT = 3000;
 
-var hrm = null;
 var plug = null;
 
 var sock = null;
@@ -31,94 +43,164 @@ var sock = null;
 var initDev = 0;
 
 var init = false;
-var currHR = null;
 var isOn = false;
 
 const client = new Client();
 
+const wsChannels = [ 'device', 'fan', 'hrm' ];
+
+var discoveredIoTDevices = {};
+var discoveredBLEDevices = {};
+
+var selectedActuator = null;
+var selectedSensor = null;
+
+function wsNotify(channel, data) {
+    if(sock == null) return;
+    if(wsChannels.includes(channel)) {
+        sock.emit(channel, data);
+    }
+}
+
+function sendIoTDeviceList() {
+    for(var i in discoveredIoTDevices) {
+        var info = discoveredIoTDevices[i]['info'];
+        if(info == null) continue;
+        wsNotify('device', {
+            'type'  : 'actuator',
+            'id'    : info.deviceId,
+            'name'  : info.alias
+        });
+    }
+}
+
+function sendBLEDeviceList() {
+    for(uuid in discoveredBLEDevices) {
+        var peripheral = discoveredBLEDevices[uuid]['peripheral'];
+        wsNotify('device', {
+            'type'  : 'sensor',
+            'id'    : peripheral.uuid,
+            'name'  : peripheral.advertisement.localName
+        });
+    }
+}
+
 function switchOnFan() {
     if(plug != null && !isOn) {
         plug.setPowerState(true);
-        if(sock != null) sock.emit('fan', { 'err' : null, 'data' : 'on' });
     }
 }
 
 function switchOffFan() {
     if(plug != null && isOn) {
         plug.setPowerState(false);
-        if(sock != null) sock.emit('fan', { 'err' : null, 'data' : 'off' });
     }
 }
 
-function checkInit(devMask) {
-    initDev |= devMask;
-    init = (initDev == 3);
-    if(init) console.log('Devices initialized.');
+function getIoTDeviceState(deviceId) {
+    if(deviceId in discoveredIoTDevices) {
+        discoveredIoTDevices[deviceId]['device'].getSysInfo().then(function(info) {
+            discoveredIoTDevices[deviceId]['info'] = info;
+            if(info.relay_state !== undefined) {}
+        });
+    }
 }
 
+function enumerateDevices() {
+    console.log('Following devices were discovered:');
+    for(var i in discoveredIoTDevices) {
+        var info = discoveredIoTDevices[i]['info'];
+        var devType = (info.mic_type == undefined) ? info.type : info.mic_type;
+        console.log(devType + '\t: ' + info.model + ' (' + info.alias + ')');
+    }
+    sendIoTDeviceList();
+}
+
+setTimeout(enumerateDevices, 3000);
 client.startDiscovery().on('device-new', (device) => {
-    if(device.deviceId === FAN_PLUG_DEVID) {
-        console.log('Fan smart plug found!');
-        checkInit(DEVMASK_PLUG);
-        plug = device;
-        if(plug._sysInfo.relay_state == 1) isOn = true;
-    }
-    device.getSysInfo().then(function(data) {
-        devInfoFile = 'devices/' + device.deviceId + '.json';
-        fs.writeFileSync(devInfoFile, JSON.stringify(data, null, 4));
-    });
+    discoveredIoTDevices[device.deviceId] = {
+        'device'    : device,
+        'info'      : null
+    };
+    getIoTDeviceState(device.deviceId);
 });
-
-console.log('Discovery started.');
 
 noble.on('scanStart', function() {
     console.log('Scanning for devices...');
 });
 
-noble.on('discover', function(peripheral) {
-    if(peripheral.uuid === HRM_DUAL_UUID) {
-        console.log('Discovered HRM peripheral.');
-        checkInit(DEVMASK_HRM);
-        hrm = peripheral;
-        noble.stopScanning();
-        hrm.connect(function(err) {
+function monitor() {}
+
+function processHRData(data) {
+    var buflen = data.length;
+    var hrate = (buflen == 2) ? data.readInt16BE() : (data.readInt32BE() & 0x00FF0000) >> 16;
+    if(DEBUG) console.log(`Heart Rate ${hrate} BPM`);
+    wsNotify('hrm', {
+        'hr'    : hrate
+    });
+}
+
+var characteristicDataCallbacks = {
+    '2a37'  : processHRData
+};
+
+function isBLEPeripheralConnected(peripheral) {
+    var isConnected = (peripheral.state === 'connected');
+    if(peripheral.uuid in discoveredBLEDevices) discoveredBLEDevices[peripheral.uuid]['isConnected'] = isConnected;
+    return isConnected;
+}
+
+function subscribeBLECharacteristic(characteristic) {
+    if(characteristic.uuid in characteristicDataCallbacks) {
+        characteristic.on('data', characteristicDataCallbacks[characteristic.uuid]);
+        characteristic.subscribe(function(err) {
             if(err != null) console.log(err);
-            hrm.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
-                for(var i in characteristics) {
-                    var c = characteristics[i];
-                    var suuid = c._serviceUuid.split('-')[0];
-                    var cuuid = c.uuid.split('-')[0];
-                    var properties = c.properties;
-                    if(suuid.indexOf(SERVICE_UUID_HRM) >= 0 && cuuid.indexOf(CHARACTERISTIC_UUID_HRM) >= 0 && properties.includes('notify')) {
-                        console.log('subscribing to hrm characteristic.');
-                        c.on('data', function(data) {
-                            if(!init) {
-                                return;
-                            }
-                            var buflen = data.length;
-                            var hrate = (buflen == 2) ? data.readInt16BE() : (data.readInt32BE() & 0x00FF0000) >> 16;
-                            if(sock != null) {
-                                sock.emit('hr', { 'err' : null, 'data' : hrate });
-                                sock.emit('fan', { 'err' : null, 'data' : (isOn ? 'on' : 'off') });
-                            }
-                            if(hrate != currHR && hrate < HR_MAX && hrate > 0) {
-                                currHR = hrate;
-                                if(hrate > HR_THRESH_HIGH) {
-                                    console.log('Heart rate increased to workout zone:', hrate);
-                                    switchOnFan();
-                                } else if(hrate < HR_THRESH_LOW) {
-                                    console.log('Heart rate dropped to recovery zone:', hrate);
-                                    switchOffFan();
-                                }
-                            }
-                        });
-                        c.subscribe(function(err) {
-                            if(err != null) console.log(err);
-                        });
+            else {
+                console.log(`Subscribed to ${SERVICE_UUIDS[characteristic._serviceUuid]} characteristic ${CHARACTERISTIC_UUIDS[characteristic.uuid]}`);
+            }
+        });
+    }
+}
+
+function connectBLEPeripheral(peripheral) {
+    peripheral.connect(function(err) {
+        if(err != null) console.log(err);
+        else {
+            peripheral.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
+                if(error != null) console.log(error);
+                else {
+                    for(var i in characteristics) {
+                        if(characteristics[i].uuid in CHARACTERISTIC_UUIDS) {
+                            discoveredBLEDevices[peripheral.uuid]['characteristics'][characteristics[i].uuid] = characteristics[i];
+                            console.log(`[${peripheral.advertisement.localName}] characteristic ${CHARACTERISTIC_UUIDS[characteristics[i].uuid]}`);
+                            subscribeBLECharacteristic(characteristics[i]);
+                        }
                     }
                 }
             });
-        });
+            isBLEPeripheralConnected(peripheral);
+        }
+    });
+}
+
+noble.on('discover', function(peripheral) {
+    if(peripheral.uuid in discoveredBLEDevices) return;
+    if(!peripheral.connectable) return;
+    for(var i in peripheral.advertisement.serviceUuids) {
+        var uuid = peripheral.advertisement.serviceUuids[i];
+        if(uuid in SERVICE_UUIDS) {
+            console.log(`Discovered ${SERVICE_DESCRIPTIONS[SERVICE_UUIDS[uuid]]} ${peripheral.advertisement.localName}`)
+            discoveredBLEDevices[peripheral.uuid] = {
+                'peripheral'        : peripheral,
+                'characteristics'   : {},
+                'isConnected'       : false
+            };
+            wsNotify('device', {
+                'type'  : 'sensor',
+                'id'    : peripheral.uuid,
+                'name'  : peripheral.advertisement.localName
+            });
+        }
     }
 });
 
@@ -171,8 +253,17 @@ const io = new Server(server);
 
 io.on('connection', function(socket) {
     sock = socket;
-    sock.emit('fan', { 'err' : null, 'data' : (isOn ? 'on' : 'off') });
     console.log('Connected to web interface.');
+    sendIoTDeviceList();
+    sendBLEDeviceList();
+    sock.on('monitor', function(data) {
+        selectedSensor = data.sensor;
+        selectedActuator = data.actuator;
+        if(selectedSensor in discoveredBLEDevices) {
+            var peripheral = discoveredBLEDevices[selectedSensor]['peripheral'];
+            connectBLEPeripheral(peripheral);
+        }
+    });
 });
 
 server.listen(LISTEN_PORT, function() {
